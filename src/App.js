7a303ceb8dd0f6f346636deb90a8af3973908d1e
gaboe <gabrielecegi@gmail.com>
2018-11-20 23:27:25 +0100

    Make Zdenek Mikel happy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  let [value, state] = useState("borec");
+  return (
+    <>
+      <div>Zdenek Mikel je {value}</div>
+      <input onChange={e => state(e.target.value)} />
+    </>
+  );
 }
-
-export default App;
